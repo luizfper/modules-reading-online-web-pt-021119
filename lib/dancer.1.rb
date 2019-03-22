51815f119f7863738b3605f9c42f43f07c838222
luizfper <luizfper@gmail.com>
2019-03-22 16:25:05 -0400

    Done.

--- lib/dancer.1.rb
+++ lib/dancer.1.rb
@@ -1,11 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-require_relative './fancy_dance.rb'
-
+require_relative './fancy_dancer.rb'
 
 class Dancer
-    extend FancyDance::ClassMethods
-    include FancyDance::InstanceMethods
+    extend MetaDancing
+    include Dance
     attr_accessor :name
  
     def initialize(name)
